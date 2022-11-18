3c43c730e9790968b0d299763673e712fefd7293
barathprathosh <barath@riverstonetech.com>
2022-11-18 08:11:27 +0530

    Custom Button added for Sync Item and customer

--- opencart_integration/opencart_integration/doctype/opencart_settings/opencart_settings.js
+++ opencart_integration/opencart_integration/doctype/opencart_settings/opencart_settings.js
@@ -2,7 +2,41 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Opencart Settings', {
-	// refresh: function(frm) {
-
-	// }
+	refresh: function(frm) {
+		if (frm.doc.enable){
+			frm.trigger("enable_custom_button");
+		}		
+	},
+	enable:function(frm){
+		if (frm.doc.enable){
+			frm.trigger("enable_custom_button");
+		}
+		else{
+			frm.trigger("disable_custom_button");
+		}
+	},
+	enable_custom_button:function(frm){
+		frm.add_custom_button(__("Sync Item"), function() {
+			frappe.call({
+				method:"opencart_integration.item.item.fetch_oc_items",
+				args:{
+				},
+				callback: function(r){
+				}
+			})
+		});
+		frm.add_custom_button(__("Sync Customer"), function() {
+			frappe.call({
+				method:"opencart_integration.customer.customer.fetch_oc_customers",
+				args:{
+				},
+				callback: function(r){
+				}
+			})
+		});
+	},
+	disable_custom_button:function(frm){
+		frm.remove_custom_button(__("Sync Item"));
+		frm.remove_custom_button(__("Sync Customer"));
+	}
 });
